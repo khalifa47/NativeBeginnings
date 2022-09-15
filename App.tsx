08be64ffe61b76859cd07d47488746d5df6ea64a
khalifa47 <khalifafumo5@gmail.com>
2022-09-15 17:50:18 +0300

    in practise: buttons, inputs and views

--- App.tsx
+++ App.tsx
@@ -1,10 +1,41 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [time, setTime] = React.useState<string>("night");
+  const [num, setNum] = React.useState<number>(1);
+
+  const handleChangeTime = () => {
+    switch (time) {
+      case "night":
+        setTime("day");
+        break;
+      case "day":
+        setTime("night");
+        break;
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Do not go gentle into that good night!</Text>
+      <Text>Do not go gentle into that good {time}!</Text>
+      <Text>
+        My favorite number is <Text style={styles.styleText}>{num}</Text>
+      </Text>
+      <View style={styles.button}>
+        <Button title="Change Time" onPress={handleChangeTime} />
+        <TextInput
+          keyboardType="decimal-pad"
+          style={styles.input}
+          placeholder={"Favorite number"}
+          onChangeText={(val) => {
+            if (parseInt(val)) {
+              setNum(parseInt(val));
+            }
+          }}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +47,17 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    marginTop: 10,
+  },
+  styleText: {
+    color: "red",
+  },
+  input: {
+    borderRadius: 6,
+    backgroundColor: "#d1d1d1",
+    margin: 10,
+    padding: 10,
   },
 });
